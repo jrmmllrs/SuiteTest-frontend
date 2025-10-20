58af1dacbaa44b7878d9a1d382cb4b4f5ac19fce
jrmmllrs <jmillares0945@gmail.com>
2025-10-20 22:18:41 +0800

    qweqw

--- src/constants/views.js
+++ src/constants/views.js
@@ -15,7 +15,7 @@ export const VIEWS = {
   USER_MANAGEMENT: 'user-management', // ADD THIS LINE
 };
 
-export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 export const STORAGE_KEYS = {
   TOKEN: "token",
